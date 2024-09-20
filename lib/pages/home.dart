91b7efa4431d5805d8aa60b89d6c2de78508f635
deathmasia <halo2ofdeath@gmail.com>
2024-09-20 19:52:01 -0400

    enable scroll control

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -344,7 +344,7 @@ class _launcherState extends State<launcher>{
                       ],
                     ), 
                     onVerticalDragStart: (details) {
-                      showModalBottomSheet<void>(showDragHandle: true ,context: context, builder: (BuildContext context) {
+                      showModalBottomSheet<void>(isScrollControlled: true ,showDragHandle: true ,context: context, builder: (BuildContext context) {
                         return Padding(
                           padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
                           child: 
