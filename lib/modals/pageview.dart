35d0d5d7b08d1eb7d85a24687746501f5a9153ce
deathmasia <halo2ofdeath@gmail.com>
2024-10-03 21:49:31 -0400

    Fix reload on textfield focus and implemnt a jump to last known page of the pageview

--- lib/modals/pageview.dart
+++ lib/modals/pageview.dart
@@ -8,6 +8,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 class pages extends StatefulWidget {
   pages(this.prefs,{super.key});
   SharedPreferences prefs;
+
   @override
   State<pages> createState() => _pagesState();
 }
@@ -15,8 +16,9 @@ class pages extends StatefulWidget {
 class _pagesState extends State<pages> {
   List<Widget> initialItems = [];
   late WidgetList widgets = WidgetList(widgets: initialItems, prefs: widget.prefs);
-  List<bool> visibilityStates = [true, true, true, true];
   int lastPage = 0;
+  PageController _pageController = PageController();
+
   @override
   void initState() {
     initialItems = widgets.getWidgets();
@@ -24,55 +26,45 @@ class _pagesState extends State<pages> {
     super.initState();
   }
 
-  void loadPrefs(){
+  void loadPrefs()async{
     int? restoreLastPage = widget.prefs.getInt("Page");
-    if (restoreLastPage != null){
-      setState(() {
-        lastPage = restoreLastPage;
+    if (restoreLastPage != 0){
+      await Future.delayed(const Duration(milliseconds: 1));
+      setState(()  {
+        lastPage = restoreLastPage!;
+        _pageController.jumpToPage(lastPage);
       });
     }
   }
 
   @override
   Widget build(BuildContext context) {
-     final visibilityState = Provider.of<WidgetVisibilityState>(context);
-
-    Future<List<Widget>> getVisibleWidgets() async {
+    
+    getVisibleWidgets() {
+      final visibilityState = Provider.of<WidgetVisibilityState>(context);
       return visibilityState.order
         .where((index) => visibilityState.visibility[index])
         .map((index) => initialItems[index])
         .toList();
     }
-
-    return FutureBuilder<List<Widget>>(
-      future: getVisibleWidgets(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Widgetoptions(widget.prefs);
-        } else if (snapshot.hasError) {
-          return Text('Error: ${snapshot.error}');
-        } else {
-          List<Widget> visibleWidgets = snapshot.data ?? [];
-          PageController _pageController = PageController(initialPage: lastPage);
-          return Padding(
-            padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-            child: SizedBox(
-              height: 500,
-              child: PageView(
-                controller: _pageController,
-                onPageChanged: (int page){
-                  lastPage = page;
-                  widget.prefs.setInt("Page", page);
-                },
-                children: [
-                  Widgetoptions(widget.prefs),
-                  ...visibleWidgets,
-                ],
-              ),
-            ),
-          );
-        }
-      }
+    _pageController = PageController(initialPage: lastPage);
+  
+    return Padding(
+      padding: EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+      child: SizedBox(
+        height: 500,
+        child: PageView(
+          controller: _pageController,
+          onPageChanged: (int page){
+            lastPage = page;
+            widget.prefs.setInt("Page", page);
+          },
+          children: [
+            Widgetoptions(widget.prefs),
+            ...getVisibleWidgets(),
+          ],
+        ),
+      ),
     );
   }
 }
